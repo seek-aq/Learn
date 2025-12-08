7154b676c6da4e5de3ccf9610b62b9b40f01750f
Scccc7 <suchen.0799ng@qq.com>
2025-12-08 16:20:34 +0800

    Add multiplication function to calculator

--- 2025_11_27.c
+++ 2025_11_27.c
@@ -13,10 +13,14 @@ int add(int a,int b)
 {
        return a+b;
 }
+int multiply(int a,int b)
+{
+       return a*b;
+}
 int main()
 {
        int a,b;
-       printf("请输入你要计算的两个整数!");
+       printf("请输入你要计算的两个整数!);
        scanf("%d %d",&a,&b);
        printf("%d-%d=%d\n",a,b,subtraction(a,b));
        printf("%.2f/%.2f=%.2f\n",a,b,chufa(a,b));
